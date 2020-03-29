bf38612d65b40d0fd6c71c2c9e4c5ebde648ce2a
Lucas Sloan <lucas.sloan@gmail.com>
2020-03-29 14:12:52 -0700

    Add a script to visualize the waymo bounding box labels.

---
+++ visualize_boxes.py
@@ -0,0 +1,71 @@
+import os
+import tensorflow as tf
+import math
+import numpy as np
+import itertools
+
+tf.enable_eager_execution()
+
+from waymo_open_dataset.utils import range_image_utils
+from waymo_open_dataset.utils import transform_utils
+from waymo_open_dataset.utils import  frame_utils
+from waymo_open_dataset import dataset_pb2 as open_dataset
+from waymo_open_dataset import label_pb2 as open_label
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+FILENAME = '/mnt/Bulk/Waymo/segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord'
+dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
+for data in dataset:
+    frame = open_dataset.Frame()
+    frame.ParseFromString(bytearray(data.numpy()))
+    break
+
+(range_images, camera_projections,
+ range_image_top_pose) = frame_utils.parse_range_image_and_camera_projection(
+    frame)
+
+color_map = {
+  open_label.Label.TYPE_UNKNOWN: 'grey',
+  open_label.Label.TYPE_VEHICLE: 'red',
+  open_label.Label.TYPE_PEDESTRIAN: 'green',
+  open_label.Label.TYPE_SIGN: 'blue',
+  open_label.Label.TYPE_CYCLIST: 'yellow',
+}
+
+def show_camera_image(camera_image, camera_labels, layout, cmap=None):
+  """Show a camera image and the given camera labels."""
+
+  ax = plt.subplot(*layout)
+
+  # Draw the camera labels.
+  for camera_labels in frame.camera_labels:
+    # Ignore camera labels that do not correspond to this camera.
+    if camera_labels.name != camera_image.name:
+      continue
+
+    # Iterate over the individual labels.
+    for label in camera_labels.labels:
+      # Draw the object bounding box.
+
+      ax.add_patch(patches.Rectangle(
+        xy=(label.box.center_x - 0.5 * label.box.length,
+            label.box.center_y - 0.5 * label.box.width),
+        width=label.box.length,
+        height=label.box.width,
+        linewidth=1,
+        edgecolor=color_map[label.type],
+        facecolor='none'))
+
+  # Show the camera image.
+  plt.imshow(tf.image.decode_jpeg(camera_image.image), cmap=cmap)
+  plt.title(open_dataset.CameraName.Name.Name(camera_image.name))
+  plt.grid(False)
+  plt.axis('off')
+  plt.show()
+
+plt.figure(figsize=(25, 20))
+
+for index, image in enumerate(frame.images):
+  show_camera_image(image, frame.camera_labels, [3, 3, index+1])
